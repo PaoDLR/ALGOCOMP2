0f13eaefa903de7d3cbd3409b23f3d6d50adeba6
PaoDLR <36217674+PaoDLR@users.noreply.github.com>
2019-06-24 17:46:08 +0800

    Skeleton of the things needed to do

---
+++ ALGOCOMP2/src/algocomp2/ALGOCOMP2.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algocomp2;
+
+/**
+ *
+ * @author Paolo Delos Reyes & Miguel Rivera
+ */
+public class ALGOCOMP2 {
+
+    /**
+     * @param input
+     */
+    
+    //item number 1
+    public static void analyze (String input) {   
+
+    //put your code here and the print statements for the output.
+        
+    }
+    
+    //item number 2
+    public static void countSwaps (int length, int[] carriageNumbers) {   
+
+    //put your code here and the print statements for the output.
+        
+    }
+    
+    //item number 3
+    public static void checkHarvest (int numVillages, int costToFeed, int[] harvests) {   
+
+    //put your code here and the print statements for the output.
+        
+    }
+    
+    //item number 4
+    public static void printCombination (int N, int K) {   
+
+    //put your code here and the print statements for the output.
+        
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
